6002695f7d9ad24225512f957c99694b22e94efe
el-wrk <eleanor.bill@gtb.com>
2021-11-10 14:48:07 +0000

    Update allocator.R

--- R/R/allocator.R
+++ R/R/allocator.R
@@ -152,7 +152,7 @@ robyn_allocator <- function(robyn_object = NULL,
   
   #####################################
   #### Set local environment
-
+  
   ## collect input
   if (!is.null(robyn_object)) {
     if (!file.exists(robyn_object)) {
@@ -162,7 +162,7 @@ robyn_allocator <- function(robyn_object = NULL,
       objectPath <- dirname(robyn_object)
       objectName <- sub("'\\..*$", "", basename(robyn_object))
     }
-
+    
     select_build_all <- 0:(length(Robyn) - 1)
     if (is.null(select_build)) {
       select_build <- max(select_build_all)
@@ -173,11 +173,11 @@ robyn_allocator <- function(robyn_object = NULL,
         " for the response function. select_build = 0 selects initial model, 1 the first refresh etc"
       )
     }
-
+    
     if (!(select_build %in% select_build_all) | length(select_build) != 1) {
       stop("select_build must be one value of ", paste(select_build_all, collapse = ", "))
     }
-
+    
     listName <- ifelse(select_build == 0, "listInit", paste0("listRefresh", select_build))
     InputCollect <- Robyn[[listName]][["InputCollect"]]
     OutputCollect <- Robyn[[listName]][["OutputCollect"]]
@@ -185,9 +185,9 @@ robyn_allocator <- function(robyn_object = NULL,
   } else if (any(is.null(InputCollect), is.null(OutputCollect), is.null(select_model))) {
     stop("when robyn_object is not provided, then InputCollect, OutputCollect, select_model must be provided")
   }
-
+  
   message(paste(">>> Running budget allocator for model ID", select_model, "..."))
-
+  
   ## get data & params
   dt_input <- InputCollect$dt_input
   dt_mod <- InputCollect$dt_mod
@@ -201,16 +201,16 @@ robyn_allocator <- function(robyn_object = NULL,
   endRW <- InputCollect$rollingWindowEndWhich
   adstock <- InputCollect$adstock
   spendExpoMod <- InputCollect$modNLSCollect
-
+  
   dt_hyppar <- OutputCollect$resultHypParam[solID == select_model]
   if (!(select_model %in% dt_hyppar$solID)) {
     stop("provided select_model is not within the best results")
   }
-
+  
   dt_bestCoef <- OutputCollect$xDecompAgg[solID == select_model & rn %in% InputCollect$paid_media_vars]
-
+  
   ## check input parameters
-
+  
   if (any(channel_constr_low < 0.01)) {
     stop("'channel_constr_low' must be >= 0.01")
   }
@@ -223,24 +223,24 @@ robyn_allocator <- function(robyn_object = NULL,
   if (!(scenario %in% c("max_historical_response", "max_response_expected_spend"))) {
     stop("scenario must be 'max_historical_response', 'max_response_expected_spend'")
   }
-
+  
   if (length(channel_constr_up) != 1) {
     if (length(channel_constr_low) != length(InputCollect$paid_media_vars) |
-      length(channel_constr_up) != length(InputCollect$paid_media_vars)) {
+        length(channel_constr_up) != length(InputCollect$paid_media_vars)) {
       stop(paste(
         "'channel_constr_low' & 'channel_constr_up' have to contain either only 1",
         "value or have same length as 'InputCollect$paid_media_vars'"
       ))
     }
   }
-
+  
   names(channel_constr_low) <- paid_media_vars
   names(channel_constr_up) <- paid_media_vars
-
+  
   ## filter and sort
-
+  
   dt_mediaSpend <- dt_input[startRW:endRW, mediaSpendSorted, with = FALSE]
-
+  
   ## sort table and get filter for channels mmm coef reduced to 0
   dt_coef <- dt_bestCoef[, .(rn, coef)]
   get_rn_order <- order(dt_bestCoef$rn)
@@ -248,7 +248,7 @@ robyn_allocator <- function(robyn_object = NULL,
   dt_bestCoef <- dt_bestCoef[get_rn_order]
   coefSelectorSorted <- dt_coefSorted[, coef > 0]
   names(coefSelectorSorted) <- dt_coefSorted$rn
-
+  
   ## filter and sort all variables by name that is essential for the apply function later
   mediaVarSortedFiltered <- mediaVarSorted[coefSelectorSorted]
   mediaSpendSortedFiltered <- mediaSpendSorted[coefSelectorSorted]
@@ -256,18 +256,18 @@ robyn_allocator <- function(robyn_object = NULL,
     chn_coef0 <- setdiff(mediaVarSorted, mediaVarSortedFiltered)
     message(paste(chn_coef0, collapse = ", "), " are excluded in optimiser because their coeffients are 0")
   }
-
+  
   dt_hyppar <- dt_hyppar[, .SD, .SDcols = na.omit(
     str_extract(names(dt_hyppar), paste(paste0(mediaVarSortedFiltered, ".*"), collapse = "|"))
   )]
   setcolorder(dt_hyppar, sort(names(dt_hyppar)))
-
+  
   dt_optim <- dt_mod[, mediaVarSortedFiltered, with = FALSE]
   dt_optimCost <- dt_input[startRW:endRW, mediaSpendSortedFiltered, with = FALSE]
   dt_bestCoef <- dt_bestCoef[rn %in% mediaVarSortedFiltered]
-
+  
   costMultiplierVec <- InputCollect$mediaCostFactor[mediaVarSortedFiltered]
-
+  
   if (any(InputCollect$costSelector)) {
     dt_modNLS <- merge(data.table(channel = mediaVarSortedFiltered), spendExpoMod, all.x = TRUE, by = "channel")
     vmaxVec <- dt_modNLS[order(rank(channel))][, Vmax]
@@ -278,26 +278,26 @@ robyn_allocator <- function(robyn_object = NULL,
     vmaxVec <- rep(0, length(mediaVarSortedFiltered))
     kmVec <- rep(0, length(mediaVarSortedFiltered))
   }
-
+  
   costSelectorSorted <- InputCollect$costSelector[media_order]
   costSelectorSorted <- costSelectorSorted[coefSelectorSorted]
   costSelectorSortedFiltered <- costSelectorSorted[mediaVarSortedFiltered]
-
+  
   channelConstrLowSorted <- channel_constr_low[media_order][coefSelectorSorted]
   channelConstrUpSorted <- channel_constr_up[media_order][coefSelectorSorted]
-
+  
   ## get adstock parameters for each channel
   if (InputCollect$adstock == "geometric") {
     getAdstockHypPar <- unlist(dt_hyppar[, .SD, .SDcols = na.omit(str_extract(names(dt_hyppar), ".*_thetas"))])
   } else if (InputCollect$adstock %in% c("weibull_cdf", "weibull_pdf")) {
     getAdstockHypPar <- unlist(dt_hyppar[, .SD, .SDcols = na.omit(str_extract(names(dt_hyppar), ".*_shapes|.*_scales"))])
   }
-
+  
   ## get hill parameters for each channel
   hillHypParVec <- unlist(dt_hyppar[, .SD, .SDcols = na.omit(str_extract(names(dt_hyppar), ".*_alphas|.*_gammas"))])
   alphas <- hillHypParVec[str_which(names(hillHypParVec), "_alphas")]
   gammas <- hillHypParVec[str_which(names(hillHypParVec), "_gammas")]
-
+  
   chnAdstocked <- OutputCollect$mediaVecCollect[
     type == "adstockedMedia" & solID == select_model, mediaVarSortedFiltered,
     with = FALSE
@@ -309,11 +309,11 @@ robyn_allocator <- function(robyn_object = NULL,
   x = chnAdstocked
   )
   names(gammaTrans) <- names(gammas)
-
+  
   coefs <- dt_coef[, coef]
   names(coefs) <- dt_coef[, rn]
   coefsFiltered <- coefs[mediaVarSortedFiltered]
-
+  
   ## build evaluation funciton
   if (any(InputCollect$costSelector)) {
     mm_lm_coefs <- spendExpoMod$coef_lm
@@ -321,11 +321,11 @@ robyn_allocator <- function(robyn_object = NULL,
   } else {
     mm_lm_coefs <- c()
   }
-
+  
   # sl=4;coeff = coefsFiltered[sl]; alpha = alphas[sl]; gammaTran = gammaTrans[sl]; chnName = mediaVarSortedFiltered[sl]; vmax = vmaxVec[sl]; km = kmVec[sl]; criteria = costSelectorSortedFiltered[sl]
   # coeff* saturation_hill(x=chnAdstocked[, get(chnName)], alpha = alpha, gamma = gammas[sl], x_marginal = mic_men(x=256198.38, Vmax=vmax, Km=km))
   # coeff* saturation_hill(x=chnAdstocked[, get(chnName)], alpha = alpha, gamma = gammas[sl], x_marginal =257771.9)
-
+  
   eval_f <- function(X) {
     return(
       list(
@@ -342,16 +342,16 @@ robyn_allocator <- function(robyn_object = NULL,
             } else {
               xScaled <- x
             }
-
+            
             # adstock scales
             xAdstocked <- xScaled #* adstockMultiplier
-
+            
             # hill transformation
             # xOut <- coeff * sum( (1 + gammaTran**alpha / (x/costMultiplier*adstockMultiplier) **alpha)**-1)
             xOut <- coeff * sum((1 + gammaTran**alpha / xAdstocked**alpha)**-1)
             xOut
-
-
+            
+            
             return(xOut)
           },
           x = X # , costMultiplier = costMultiplierVec, adstockMultiplier=adstockMultiplierVec
@@ -375,13 +375,13 @@ robyn_allocator <- function(robyn_object = NULL,
             } else {
               xScaled <- x
             }
-
+            
             # adstock scales
             xAdstocked <- xScaled #* adstockMultiplier
-
+            
             xOut <- -coeff * sum((alpha * (gammaTran**alpha) * (xAdstocked**(alpha - 1))) /
-              (xAdstocked**alpha + gammaTran**alpha)**2)
-
+                                   (xAdstocked**alpha + gammaTran**alpha)**2)
+            
             return(xOut)
           },
           x = X # , costMultiplier = costMultiplierVec, adstockMultiplier=adstockMultiplierVec
@@ -392,13 +392,13 @@ robyn_allocator <- function(robyn_object = NULL,
           SIMPLIFY = TRUE
           )
         ), # https://www.derivative-calculator.net/ on the objective function 1/(1+gamma^alpha / x^alpha)
-
+        
         "objective.channel" =
           mapply(function(x # , costMultiplier, adstockMultiplier
                           , coeff,
                           alpha, gammaTran,
                           chnName, vmax, km, criteria) {
-
+            
             # apply Michaelis Menten model to scale spend to exposure
             if (criteria) {
               xScaled <- mic_men(x = x, Vmax = vmax, Km = km) # vmax * x / (km + x)
@@ -407,13 +407,13 @@ robyn_allocator <- function(robyn_object = NULL,
             } else {
               xScaled <- x
             }
-
+            
             # adstock scales
             xAdstocked <- xScaled #* adstockMultiplier
-
+            
             # xOut <- -coeff * sum( (1 + gammaTran**alpha / (x/costMultiplier*adstockMultiplier) **alpha)**-1)
             xOut <- -coeff * sum((1 + gammaTran**alpha / xAdstocked**alpha)**-1)
-
+            
             return(xOut)
           },
           x = X # , costMultiplier = costMultiplierVec, adstockMultiplier=adstockMultiplierVec
@@ -426,13 +426,13 @@ robyn_allocator <- function(robyn_object = NULL,
       )
     )
   }
-
+  
   # eval_f(c(1,1))
-
+  
   ## build contraints function with scenarios
   nPeriod <- nrow(dt_optimCost)
   xDecompAggMedia <- OutputCollect$xDecompAgg[solID == select_model & rn %in% InputCollect$paid_media_vars][order(rank(rn))]
-
+  
   if (scenario == "max_historical_response") {
     expected_spend <- sum(xDecompAggMedia$total_spend)
     expSpendUnitTotal <- sum(xDecompAggMedia$mean_spend) # expected_spend / nPeriod
@@ -449,28 +449,22 @@ robyn_allocator <- function(robyn_object = NULL,
   names(histSpend) <- sort(InputCollect$paid_media_vars)
   # histSpend <- colSums(dt_optimCost)
   histSpendTotal <- sum(histSpend)
-  print('test')
-  print(c('histSpendTotal', histSpendTotal))
-  
   histSpendUnitTotal <- sum(xDecompAggMedia$mean_spend) # histSpendTotal/ nPeriod
   # histSpendShare <- histSpend / histSpendTotal
   # histSpendUnit <- histSpendUnitTotal * histSpendShare
   histSpendUnit <- xDecompAggMedia[rn %in% mediaVarSortedFiltered, mean_spend]
-  print(c('histSpendUnit', histSpendUnit))
-  
   names(histSpendUnit) <- mediaVarSortedFiltered
   #histSpendShare <- xDecompAggMedia[rn %in% mediaVarSortedFiltered, spend_share]
-  histSpendShare <- histSpendUnit/histSpendTotal
-  print(c('histSpendShare'), histSpendShare)
+  histSpendShare <- histSpendUnit/sum(histSpendUnit)
+  # histSpendShare <- xDecompAggMedia[rn %in% mediaVarSortedFiltered, visit_share]
   names(histSpendShare) <- mediaVarSortedFiltered
-  print(c'mediaVarSortedFiltered', mediaVarSortedFiltered)
-
+  
   # QA: check if objective function correctly implemented
   histResponseUnitModel <- xDecompAggMedia[rn %in% mediaVarSortedFiltered, get("mean_response")]
   names(histResponseUnitModel) <- mediaVarSortedFiltered
   histResponseUnitAllocator <- unlist(-eval_f(histSpendUnit)[["objective.channel"]])
   identical(round(histResponseUnitModel, 3), round(histResponseUnitAllocator, 3))
-
+  
   eval_g_eq <- function(X) {
     constr <- sum(X) - expSpendUnitTotal
     grad <- rep(1, length(X))
@@ -479,7 +473,7 @@ robyn_allocator <- function(robyn_object = NULL,
       "jacobian" = grad
     ))
   }
-
+  
   eval_g_ineq <- function(X) {
     constr <- sum(X) - expSpendUnitTotal
     grad <- rep(1, length(X))
@@ -488,12 +482,12 @@ robyn_allocator <- function(robyn_object = NULL,
       "jacobian" = grad
     ))
   }
-
+  
   ## set initial values and bounds
   lb <- histSpendUnit * channelConstrLowSorted
   ub <- histSpendUnit * channelConstrUpSorted
   x0 <- lb
-
+  
   ## set optim options
   if (optim_algo == "MMA_AUGLAG") {
     local_opts <- list(
@@ -506,14 +500,14 @@ robyn_allocator <- function(robyn_object = NULL,
       "xtol_rel" = 1.0e-10
     )
   }
-
+  
   opts <- list(
     "algorithm" = "NLOPT_LD_AUGLAG",
     "xtol_rel" = 1.0e-10,
     "maxeval" = maxeval,
     "local_opts" = local_opts
   )
-
+  
   ## run optim
   if (constr_mode == "eq") {
     nlsMod <- nloptr::nloptr(
@@ -536,13 +530,13 @@ robyn_allocator <- function(robyn_object = NULL,
       opts = opts
     )
   }
-
+  
   # print(nlsMod)
-
+  
   ## collect output
-
+  
   dt_bestModel <- dt_bestCoef[, .(rn, mean_spend, xDecompAgg, roi_total, roi_mean)][order(rank(rn))]
-
+  
   dt_optimOut <- data.table(
     channels = mediaVarSortedFiltered,
     histSpend = histSpend[mediaVarSortedFiltered],
@@ -566,23 +560,22 @@ robyn_allocator <- function(robyn_object = NULL,
     optmRoiUnit = -eval_f(nlsMod$solution)[["objective.channel"]] / nlsMod$solution,
     optmResponseUnitLift = (-eval_f(nlsMod$solution)[["objective.channel"]] / histResponseUnitModel) - 1
   )
-
+  
   dt_optimOut[, optmResponseUnitTotalLift := (optmResponseUnitTotal / initResponseUnitTotal) - 1]
-  print(c('dt_optimOut', dt_optimOut))
-
-  print(c('optmSpendShareUnit', nlsMod$solution / sum(nlsMod$solution)))
+  # print(dt_optimOut)
+  
   ## plot allocator results
-
+  
   plotDT_total <- copy(dt_optimOut) # plotDT_total <- optim_result$dt_optimOut
-
+  
   # ROI comparison plot
-
+  
   plotDT_roi <- plotDT_total[, c("channels", "initRoiUnit", "optmRoiUnit")][order(rank(channels))]
   plotDT_roi[, channels := as.factor(channels)]
   chn_levels <- plotDT_roi[, as.character(channels)]
   plotDT_roi[, channels := factor(channels, levels = chn_levels)]
   setnames(plotDT_roi, names(plotDT_roi), new = c("channel", "initial roi", "optimised roi"))
-
+  
   plotDT_roi <- suppressWarnings(melt.data.table(plotDT_roi, id.vars = "channel", value.name = "roi"))
   p11 <- ggplot(plotDT_roi, aes(x=channel, y=roi,fill = variable)) +
     geom_bar(stat = "identity", width = 0.5, position = "dodge") +
@@ -595,15 +588,15 @@ robyn_allocator <- function(robyn_object = NULL,
          ,subtitle = paste0("Total visits increase ", plotDT_total[, round(mean(optmSpendUnitTotalDelta)*100,1)], "%"
                             ,"\nTotal sales increase ", plotDT_total[, round(mean(optmResponseUnitTotalLift)*100,1)], "% with optimised visit allocation")
          ,y="", x="Channels")
-
+  
   # Response comparison plot
   plotDT_resp <- plotDT_total[, c("channels", "initResponseUnit", "optmResponseUnit")][order(rank(channels))]
   plotDT_resp[, channels := as.factor(channels)]
   chn_levels <- plotDT_resp[, as.character(channels)]
   plotDT_resp[, channels := factor(channels, levels = chn_levels)]
   setnames(plotDT_resp, names(plotDT_resp), new = c("channel", "initial response / time unit", "optimised response / time unit"))
-
-
+  
+  
   plotDT_resp <- suppressWarnings(melt.data.table(plotDT_resp, id.vars = "channel", value.name = "response"))
   p12 <- ggplot(plotDT_resp, aes(x=channel, y=response, fill = variable)) +
     geom_bar(stat = "identity", width = 0.5, position = "dodge") +
@@ -618,17 +611,14 @@ robyn_allocator <- function(robyn_object = NULL,
          )
          ,y="", x="Channels")
   
-  
-  
   # budget share comparison plot
-  
   plotDT_share <- plotDT_total[, c("channels", "initSpendShare", "optmSpendShareUnit")][order(rank(channels))]
   plotDT_share[, channels := as.factor(channels)]
   chn_levels <- plotDT_share[, as.character(channels)]
   plotDT_share[, channels := factor(channels, levels = chn_levels)]
-  setnames(plotDT_share, names(plotDT_share), new = c("channel", "initial avg.visit share", "optimised avg.visit share"))
-
-
+  setnames(plotDT_share, names(plotDT_share), new = c("channel", "initial avg.spend share", "optimised avg.spend share"))
+  
+  
   plotDT_share <- suppressWarnings(melt.data.table(plotDT_share, id.vars = "channel", value.name = "spend_share"))
   p13 <- ggplot(plotDT_share, aes(x=channel, y=spend_share, fill = variable)) +
     geom_bar(stat = "identity", width = 0.5, position = "dodge") +
@@ -642,10 +632,10 @@ robyn_allocator <- function(robyn_object = NULL,
                             ,"\nTotal sales increase ", plotDT_total[, round(mean(optmResponseUnitTotalLift)*100,1)], "% with optimised visit allocation"
          )
          ,y="", x="Channels")
-
-
+  
+  
   ## response curve
-
+  
   plotDT_saturation <- melt.data.table(OutputCollect$mediaVecCollect[
     solID == select_model & type == "saturatedSpendReversed"
   ],
@@ -668,7 +658,7 @@ robyn_allocator <- function(robyn_object = NULL,
   use.names = FALSE
   )
   setnames(dt_optimOutScurve, c("channels", "spend", "response", "type"))
-
+  
   
   p14 <- ggplot(data= plotDT_scurve, aes(x=spend, y=response, color = channel)) +
     geom_line() +
@@ -686,28 +676,28 @@ robyn_allocator <- function(robyn_object = NULL,
                             ", mape.lift = ", plotDT_scurveMeanResponse[, round(mean(mape),4)])
          ,x="Visits" ,y="response")
   
-
+  
   grobTitle <- paste0("Budget allocator optimum result for model ID ", select_model)
   g <- (p13 + p12) / p14 + plot_annotation(
     title = grobTitle, theme = theme(plot.title = element_text(hjust = 0.5))
   )
-
+  
   message("Exporting charts into file: ", paste0(OutputCollect$plot_folder, select_model, "_reallocated.png"))
   ggsave(
     filename = paste0(OutputCollect$plot_folder, select_model, "_reallocated.png"),
     plot = g,
     dpi = 400, width = 18, height = 14
   )
-
+  
   fwrite(dt_optimOut, paste0(OutputCollect$plot_folder, select_model, "_reallocated.csv"))
-
+  
   if (ui) {
     ui <- list(p12 = p12, p13 = p13, p14 = p14)
   } else {
     ui <- NULL
   }
-
+  
   listAllocator <- list(dt_optimOut = dt_optimOut, nlsMod = nlsMod, ui = ui)
-
+  
   return(listAllocator)
 }
